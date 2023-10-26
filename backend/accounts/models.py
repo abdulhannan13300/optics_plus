cbf55e75793ed1609d671ff5cdf32b32467d5b38
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-10-26 21:41:06 +0530

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,60 +1,6 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
-
-# class CustomUserManager(BaseUserManager):
-    # def create_user(self, full_name, username, email, password=None, **extra_fields):
-    #     if not email:
-    #         raise ValueError('The Email field must be set')
-    #     if not username:
-    #         raise ValueError('User must have an username')
-
-    #     user = self.model(
-    #         email = self.normalize_email(email),
-    #         username = username,
-    #         full_name = full_name,
-    #         **extra_fields
-    #     )
-    #     user.set_password(password)
-    #     user.save(using=self._db)
-    #     return user
-
-    # def create_superuser(self, full_name, username, email, password=None, **extra_fields):
-    #     user = self.create_user(
-    #         email = self.normalize_email(email),
-    #         username = username,
-    #         password = password,
-    #          full_name = full_name,
-    #         **extra_fields
-    #     )
-    #     user.is_superuser = True
-    #     user.is_staff = True
-    #     user.is_active = True
-    #     user.save(using=self._db)
-
-    #     return user
-
-
-# class CustomUser(AbstractBaseUser,PermissionsMixin):
-#     email = models.EmailField(unique=True)
-#     full_name = models.CharField(max_length=150)
-#     username = models.CharField(max_length=50, unique=True)
-#     contact_number = models.CharField(max_length=12, blank=True)
-
-#     is_active = models.BooleanField(default=False)
-#     is_admin = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-
-#     date_joined = models.DateTimeField(auto_now_add=True)
-#     last_login = models.DateTimeField(auto_now_add=True)
-#     modified_at = models.DateTimeField(auto_now=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-    
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['full_name', 'username']
-
-#     # objects = CustomUserManager()
     
 class CompanyEmployeeManager(BaseUserManager):
     def create_user(self,first_name, last_name, username, email, password=None, **extra_fields):
