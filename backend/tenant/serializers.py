b083e96e35060e29f37e9ab62f48fe5d6510d52d
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2024-01-10 17:41:38 +0530

    Add files via upload

--- backend/tenant/serializers.py
+++ backend/tenant/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from .models import ShopCustomer
+from .models import *
 
 class ShopCustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShopCustomer
+        # fields = ['id', 'name', 'description']
+        fields = '__all__'
+
+class CustomerPrescritionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerPrescription
+        # fields = ['id', 'name', 'description']
+        fields = '__all__'
+    
+
+class OrderDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderDetails
         # fields = ['id', 'name', 'description']
         fields = '__all__'
--- backend/tenant/views.py
+++ backend/tenant/views.py
@@ -1,20 +1,12 @@
 # tenants/views.py
 
-from django.shortcuts import render
-from django.http import HttpResponse
-
-from .models import ShopCustomer
-from .serializers import ShopCustomerSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
+from .models import ShopCustomer, CustomerPrescription, OrderDetails
+from .serializers import ShopCustomerSerializer, CustomerPrescriptionSerializer, OrderDetailsSerializer
 
-# Create your views here.
-# from . models import Employee
-
-def index(request):
-    return HttpResponse(f'<h1> {request.tenant} index</h1>')
 
 
 
@@ -64,8 +56,91 @@ class ShopCustomerDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class CustomerPrescriptionList(APIView):
 
+    def get(self, request, customer_id, format=None):
+        prescriptions = CustomerPrescription.objects.filter(customer_id=customer_id)
+        serializer = CustomerPrescriptionSerializer(prescriptions, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, customer_id, format=None):
+        request.data['customer'] = customer_id  # Associate prescription with the customer
+        serializer = CustomerPrescriptionSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomerPrescriptionDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return CustomerPrescription.objects.get(pk=pk)
+        except CustomerPrescription.DoesNotExist:
+            raise Http404
+
+    def get(self, request, customer_id, pk, format=None):
+        prescription = self.get_object(pk)
+        serializer = CustomerPrescriptionSerializer(prescription)
+        return Response(serializer.data)
+
+    def put(self, request, customer_id, pk, format=None):
+        prescription = self.get_object(pk)
+        serializer = CustomerPrescriptionSerializer(prescription, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, customer_id, pk, format=None):
+        prescription = self.get_object(pk)
+        prescription.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class OrderDetailsList(APIView):
+
+    def get(self, request, customer_id, format=None):
+        orders = OrderDetails.objects.filter(customer_id=customer_id)
+        serializer = OrderDetailsSerializer(orders, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, customer_id, format=None):
+        request.data['customer'] = customer_id  # Associate order detail with the customer
+        serializer = OrderDetailsSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class OrderDetailsDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return OrderDetails.objects.get(pk=pk)
+        except OrderDetails.DoesNotExist:
+            raise Http404
+
+    def get(self, request, customer_id, pk, format=None):
+        order_detail = self.get_object(pk)
+        serializer = OrderDetailsSerializer(order_detail)
+        return Response(serializer.data)
+
+    def put(self, request, customer_id, pk, format=None):
+        order_detail = self.get_object(pk)
+        serializer = OrderDetailsSerializer(order_detail, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, customer_id, pk, format=None):
+        order_detail = self.get_object(pk)
+        order_detail.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
