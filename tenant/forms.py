e21c4415d99b794781e2c8b0878b5bcc68835df5
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2024-01-22 22:15:26 +0530

    Delete tenant directory

--- tenant/forms.py
+++
@@ -1,8 +0,0 @@
-# tenants/forms.py
-from django import forms
-from tenant.models import Tenant
-
-class TenantSignupForm(forms.ModelForm):
-    class Meta:
-        model = Tenant
-        fields = ['name', 'subdomain', 'other_fields']
