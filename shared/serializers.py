1a6587286ed20a435da40e7aeda720b68c9b09aa
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2024-01-22 22:15:18 +0530

    Delete shared directory

--- shared/serializers.py
+++
@@ -1,9 +0,0 @@
-from djoser.serializers import UserCreateSerializer
-from shared.models import ShopEmployee
-User = ShopEmployee()
-
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = User
-        # fields = { 'id', 'first_name', 'last_name','username','email','password', 'designation', 'shop'}
-        fields =  '__all__'
