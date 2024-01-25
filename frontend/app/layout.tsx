d9eb6070523f33eb15f7c2016531cb2547fe1efc
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2024-01-25 17:57:12 +0530

    Account Registration Part - 1

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,28 +3,30 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Provider from "@/redux/provider";
 import { Navbar, Footer } from "@/components/common";
+import { Setup } from "@/components/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Optics Plus",
-  description: "optics plus",
+   title: "Optics Plus",
+   description: "optics plus",
 };
 
 export default function RootLayout({
-  children,
+   children,
 }: Readonly<{
-  children: React.ReactNode;
+   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Provider>
-          <Navbar />
-          <div>{children}</div>
-          <Footer />
-        </Provider>
-      </body>
-    </html>
-  );
+   return (
+      <html lang="en">
+         <body className={inter.className}>
+            <Provider>
+               <Setup />
+               <Navbar />
+               <div>{children}</div>
+               <Footer />
+            </Provider>
+         </body>
+      </html>
+   );
 }
--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
+import { apiSlice } from "./services/apiSlice";
 
 export const makeStore = () =>
-  configureStore({
-    reducer: {
-      auth: authReducer,
-    },
-    devTools: process.env.NODE_ENV !== "production",
-  });
+   configureStore({
+      reducer: {
+         [apiSlice.reducerPath]: apiSlice.reducer,
+         auth: authReducer,
+      },
+      devTools: process.env.NODE_ENV !== "production",
+   });
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
