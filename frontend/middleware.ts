c9502c89dfbd1bdeee08b476682b3b6b6fa2a4d5
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2024-03-12 23:43:38 +0530

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,114 +1,3 @@
-// import { NextRequest, NextResponse } from "next/server";
-
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all paths except for:
-//      * 1. /api routes
-//      * 2. /_next (Next.js internals)
-//      * 3. /_static (inside /public)
-//      * 4. all root files inside /public (e.g. /favicon.ico)
-//      */
-//     "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
-//   ],
-// };
-
-// export default async function middleware(req: NextRequest) {
-//   const url = req.nextUrl;
-//   let hostname = req.headers;
-//   const searchParams = url.searchParams.toString();
-//   const pathWithSearchParams = `${url.pathname}${
-//     searchParams.length > 0 ? `?${searchParams}` : ""
-//   }`;
-//   //   const hostname = req.headers.get("host");
-
-//   // Handle cases where hostname might not have dots
-//   //   const customSubDomain = hostname?.split(".")[0];
-//   const customSubDomain = hostname
-//     .get("host")
-//     ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
-//     .filter(Boolean)[0];
-//   console.log(customSubDomain);
-
-//   //   const searchParams = url.searchParams.toString();
-//   //   const path = `${url.pathname}${
-//   //     searchParams.length > 0 ? `?${searchParams}` : ""
-//   //   }`;
-
-//   if (customSubDomain) {
-//     return NextResponse.rewrite(
-//       new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
-//     );
-//   }
-
-//   //   if (
-//   //     (customSubDomain !== process.env.NEXT_PUBLIC_DOMAIN &&
-//   //       url.pathname === "/auth/login") ||
-//   //     url.pathname === "/auth/register"
-//   //   ) {
-//   //     return NextResponse.redirect(
-//   //       new URL(`${process.env.NEXT_PUBLIC_DOMAIN}${url.pathname}`, req.url)
-//   //     );
-//   //   }
-
-//   //   if (customSubDomain !== process.env.NEXT_PUBLIC_DOMAIN) {
-//   //     return NextResponse.rewrite(new URL(`/${customSubDomain}${path}`, req.url));
-//   //   }
-
-//   // If no subdomain or same domain as NEXT_PUBLIC_DOMAIN, return original request
-//   return NextResponse.next();
-// }
-// import { NextRequest, NextResponse } from "next/server";
-
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all paths except for:
-//      * 1. /api routes
-//      * 2. /_next (Next.js internals)
-//      * 3. /_static (inside /public)
-//      * 4. all root files inside /public (e.g. /favicon.ico)
-//      */
-//     "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
-//   ],
-// };
-
-// export default async function middleware(req: NextRequest) {
-//   const url = req.nextUrl;
-//   let hostname = req.headers;
-//   const searchParams = url.searchParams.toString();
-//   const pathWithSearchParams = `${url.pathname}${
-//     searchParams.length > 0 ? `?${searchParams}` : ""
-//   }`;
-
-//   const customSubDomain = hostname
-//     .get("host")
-//     ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
-//     .filter(Boolean)[0];
-
-//   // Check if the request path starts with the [domain] route
-//   const domainRouteMatcher = /^\/\[domain\](\/.*)?$/;
-//   const domainRouteMatch = pathWithSearchParams.match(domainRouteMatcher);
-
-//   if (customSubDomain) {
-//     // If there's a subdomain, rewrite the URL as before
-//     return NextResponse.rewrite(
-//       new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
-//     );
-//   } else if (domainRouteMatch) {
-//     // If there's no subdomain and the request is for a [domain] route, redirect to /not-found
-//     const remainingPath = domainRouteMatch[1] || "";
-//     return NextResponse.rewrite(
-//       new URL(
-//         `/not-found?path=${encodeURIComponent(`/[domain]${remainingPath}`)}`,
-//         req.url
-//       )
-//     );
-//   } else {
-//     // If there's no subdomain and the request is not for a [domain] route, continue the response cycle
-//     return NextResponse.next();
-//   }
-// }
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
@@ -131,27 +20,41 @@ export default async function middleware(req: NextRequest) {
   const pathWithSearchParams = `${url.pathname}${
     searchParams.length > 0 ? `?${searchParams}` : ""
   }`;
+  //   const hostname = req.headers.get("host");
 
+  // Handle cases where hostname might not have dots
+  //   const customSubDomain = hostname?.split(".")[0];
   const customSubDomain = hostname
     .get("host")
     ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
     .filter(Boolean)[0];
+  console.log(customSubDomain);
 
-  // Check if the request path starts with the [domain] route
-  const domainRouteMatcher = /^\/\[domain\](\/.*)?$/;
-  const domainRouteMatch = pathWithSearchParams.match(domainRouteMatcher);
+  //   const searchParams = url.searchParams.toString();
+  //   const path = `${url.pathname}${
+  //     searchParams.length > 0 ? `?${searchParams}` : ""
+  //   }`;
 
   if (customSubDomain) {
-    // If there's a subdomain, rewrite the URL as before
     return NextResponse.rewrite(
       new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
     );
-  } else if (domainRouteMatch) {
-    // If there's no subdomain and the request is for a [domain] route, redirect to /not-found
-    const remainingPath = domainRouteMatch[1] || "";
-    return NextResponse.rewrite(new URL(`/not-found`, req.url));
-  } else {
-    // If there's no subdomain and the request is not for a [domain] route, redirect to /not-found
-    return NextResponse.next();
   }
+
+  //   if (
+  //     (customSubDomain !== process.env.NEXT_PUBLIC_DOMAIN &&
+  //       url.pathname === "/auth/login") ||
+  //     url.pathname === "/auth/register"
+  //   ) {
+  //     return NextResponse.redirect(
+  //       new URL(`${process.env.NEXT_PUBLIC_DOMAIN}${url.pathname}`, req.url)
+  //     );
+  //   }
+
+  //   if (customSubDomain !== process.env.NEXT_PUBLIC_DOMAIN) {
+  //     return NextResponse.rewrite(new URL(`/${customSubDomain}${path}`, req.url));
+  //   }
+
+  // If no subdomain or same domain as NEXT_PUBLIC_DOMAIN, return original request
+  return NextResponse.next();
 }
